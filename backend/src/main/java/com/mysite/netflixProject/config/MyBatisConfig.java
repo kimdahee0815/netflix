603163ca4c5a3bf6346055788a4fa2efe17cfe86
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 00:09:21 +0900

    feat: mappers with xml

--- backend/src/main/java/com/mysite/netflixProject/config/MyBatisConfig.java
+++ backend/src/main/java/com/mysite/netflixProject/config/MyBatisConfig.java
@@ -17,8 +17,11 @@ public class MyBatisConfig {
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliasesPackage("com.mysite.netflixProject");
+        
+        // Use classpath* to search in all classpath locations
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
+        sessionFactory.setMapperLocations(resolver.getResources("classpath*:mappers/*.xml"));
         return sessionFactory.getObject();
     }
     
