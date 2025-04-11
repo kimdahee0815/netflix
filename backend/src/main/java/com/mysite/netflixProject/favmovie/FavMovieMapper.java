a0bcd63ef5c2ca9402fbae9342df0f8dad55711b
kimdahee0815 <billionairedahee@gmail.com>
2025-04-11 23:46:03 +0900

    fix: fix for favmoviemapper

--- backend/src/main/java/com/mysite/netflixProject/favmovie/FavMovieMapper.java
+++ backend/src/main/java/com/mysite/netflixProject/favmovie/FavMovieMapper.java
@@ -2,17 +2,8 @@ package com.mysite.netflixProject.favmovie;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
+import org.apache.ibatis.annotations.*;
 import com.mysite.netflixProject.board.Idchange;
-
-
 
 @Mapper
 public interface FavMovieMapper {
