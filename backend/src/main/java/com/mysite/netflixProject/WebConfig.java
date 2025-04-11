adbb2976a43b2dbe12094faf0d83fd431437b89f
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 07:51:28 +0900

    fix: serverlet issues and database connection

--- backend/src/main/java/com/mysite/netflixProject/WebConfig.java
+++ backend/src/main/java/com/mysite/netflixProject/WebConfig.java
@@ -6,15 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://*.netlify.app",
+            .allowedOrigins(
+                "https://*.netlify.app",
                 "http://localhost:3000",
-                "https://netflix-dmqv.onrender.com")
+                "https://netflix-dmqv.onrender.com"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
+            .allowCredentials(true)
             .maxAge(3600);
     }
 
