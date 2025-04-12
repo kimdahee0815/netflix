7cdb9e3c065c8b21e75368ad2767297b3b06f2a9
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 21:34:49 +0900

    feat: add api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_URL: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:8080'
-    : 'https://netflix-dmqv.onrender.com'
+    ? 'http://localhost:8080/api'
+    : 'https://netflix-dmqv.onrender.com/api'
 };
 
 export default config;
