5f94606e4636d4cc51dea98899e1524691585dab
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 17:42:17 +0900

    fix: add api for servlet

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_URL: process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:8080'
+    ? 'http://localhost:8080/api'
     : 'https://netflix-dmqv.onrender.com'
 };
 
