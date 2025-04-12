91a17e0e23d5780b3ea2a4805d6cd0b72976dc2a
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 10:53:10 +0900

    fix: allow origin for netlify

--- backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
+++ backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
@@ -2,11 +2,13 @@ package com.mysite.netflixProject.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     
     @Override
