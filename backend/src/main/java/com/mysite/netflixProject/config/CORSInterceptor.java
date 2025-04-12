746755bed40f7c6262529727a57c47d1bc15281d
kimdahee0815 <billionairedahee@gmail.com>
2025-04-12 12:37:19 +0900

    fix: allow origin for netlify

---
+++ backend/src/main/java/com/mysite/netflixProject/config/CORSInterceptor.java
@@ -0,0 +1,50 @@
+package com.mysite.netflixProject.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSInterceptor implements Filter {
+
+    private static final String[] allowedOrigins = {
+        "http://localhost:3000", "https://net-flix-clone-dahee-kim.netlify.app"
+    };
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        String requestOrigin = request.getHeader("Origin");
+        if(isAllowedOrigin(requestOrigin)) {
+            // Authorize the origin, all headers, and all methods
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", requestOrigin);
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods",
+                "GET, OPTIONS, HEAD, PUT, POST, DELETE");
+
+            HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+            // CORS handshake (pre-flight request)
+            if (request.getMethod().equals("OPTIONS")) {
+                resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+                return;
+            }
+        }
+        // pass the request along the filter chain
+        filterChain.doFilter(request, servletResponse);
+    }
+
+    private boolean isAllowedOrigin(String origin){
+        for (String allowedOrigin : allowedOrigins) {
+            if(origin.equals(allowedOrigin)) return true;
+        }
+        return false;
+    }
+}
