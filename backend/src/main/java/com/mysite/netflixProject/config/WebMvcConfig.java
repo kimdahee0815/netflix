055183b947c450baafdf971c5a83780ed8c73fbb
kimdahee0815 <billionairedahee@gmail.com>
2025-04-13 10:31:30 +0900

    fix: build frontend resources

--- backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
+++ backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("file:frontend/build/")
                 .setCacheControl(CacheControl.noCache());
     }
 
