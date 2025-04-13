f27167976de5092c2b3c0d90817555eccec08d81
kimdahee0815 <billionairedahee@gmail.com>
2025-04-13 11:34:02 +0900

    fix: static folder path update

--- backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
+++ backend/src/main/java/com/mysite/netflixProject/config/WebMvcConfig.java
@@ -22,7 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
-                .setViewName("forward:/index.html");
+                .setViewName("forward:/");
     }
 
     @Override
