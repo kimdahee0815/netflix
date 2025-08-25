ab81651c633ebd6efe263ec91cf5932ed3d2c403
kimdahee0815 <billionairedahee@gmail.com>
2025-08-25 17:11:24 +0900

    test: banner movie update

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,17 +1,10 @@
 import Grid from "@mui/material/Grid";
-import { useEffect } from "react";
 import Banner_data from "./Banner_data";
 import "../css/movie.css";
-import { getBannerMovie } from "../store/movie";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Banner() {
-    const dispatch = useDispatch();
     const movie = useSelector((state) => state.movie?.bannerMovie);
-
-    useEffect(() => {
-        dispatch(getBannerMovie());
-    }, [dispatch]);
 
     return (
         <div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,44 +2,41 @@ import "../components/OutputMovieGenres";
 import { useEffect, useMemo, useRef } from "react";
 import OutputMovieGenres from "../components/OutputMovieGenres";
 import Banner from "../components/Banner";
+import { getBannerMovie } from "../store/movie";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGenresMoviesData } from "../store/movie";
 
 const Home = () => {
-  const movies = useSelector((state) => state.movie.movies);
-  const dispatch = useDispatch();
-  const hasInitialized = useRef(false);
-  
-  const genres = useMemo(
-    () => ["comedy", "action", "adventure", "horror", "romance"],
-    []
-  );
+    const movies = useSelector((state) => state.movie.movies);
+    const dispatch = useDispatch();
+    const hasInitialized = useRef(false);
 
-  useEffect(() => {
-    if (hasInitialized.current) {
-      return;
-    }
+    const genres = useMemo(() => ["comedy", "action", "adventure", "horror", "romance"], []);
 
-    const allGenresLoaded = genres.every(genre => 
-      movies?.[genre] && movies?.[genre].length > 0
+    useEffect(() => {
+        if (hasInitialized.current) {
+            return;
+        }
+
+        const allGenresLoaded = genres.every((genre) => movies?.[genre] && movies?.[genre].length > 0);
+
+        if (allGenresLoaded) {
+            return;
+        }
+
+        hasInitialized.current = true;
+        dispatch(getBannerMovie());
+        dispatch(getAllGenresMoviesData(genres));
+    }, [dispatch, genres, movies]);
+
+    return (
+        <div style={{ marginTop: "100px" }}>
+            <Banner />
+            {genres.map((genre) => (
+                <OutputMovieGenres key={genre} genre={genre} />
+            ))}
+        </div>
     );
-
-    if (allGenresLoaded) {
-      return;
-    }
-
-    hasInitialized.current = true;
-    dispatch(getAllGenresMoviesData(genres));
-  }, [dispatch, genres, movies]);
-
-  return (
-    <div style={{ marginTop: "100px" }}>
-      <Banner />
-      {genres.map((genre) => (
-        <OutputMovieGenres key={genre} genre={genre} />
-      ))}
-    </div>
-  );
 };
 
 export default Home;
