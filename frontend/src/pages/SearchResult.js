4f2a04a7d5421d32bd4687b625292672174d8cd2
kimdahee0815 <kimdahee0815@gmail.com>
2026-06-28 23:34:30 +0900

    refactor: style change for grid

--- frontend/src/pages/SearchResult.js
+++ frontend/src/pages/SearchResult.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Grid from "@mui/material/Grid";
 import Movie from "../components/Movie";
 import { getAllSearchMovies } from "../store/movie";
 
@@ -31,36 +30,30 @@ const SearchResult = ({ search }) => {
                 >
                     Searching...
                 </div>
+            ) : searchMovies?.length === 0 ? (
+                <div
+                    style={{
+                        marginLeft: "30px",
+                        marginTop: "100px",
+                        color: "white",
+                        fontSize: "2.4em",
+                    }}
+                >
+                    No results found!
+                </div>
             ) : (
-                <div style={{ marginTop: "100px" }}>
-                    <Grid container spacing={2} justifyContent="center">
-                        {searchMovies?.length === 0 ? (
-                            <div
-                                style={{
-                                    marginLeft: "30px",
-                                    color: "white",
-                                    marginTop: "20px",
-                                    fontSize: "2.4em",
-                                }}
-                            >
-                                No results found!
-                            </div>
-                        ) : (
-                            <div
-                                style={{
-                                    marginTop: "100px",
-                                    padding: "0 40px",
-                                    display: "grid",
-                                    gridTemplateColumns: "repeat(auto-fill, minmax(180px, 1fr))",
-                                    gap: "16px",
-                                }}
-                            >
-                                {searchMovies?.map((movie) => (
-                                    <Movie {...movie} key={movie.id} />
-                                ))}
-                            </div>
-                        )}
-                    </Grid>
+                <div
+                    style={{
+                        marginTop: "100px",
+                        padding: "0 40px",
+                        display: "grid",
+                        gridTemplateColumns: "repeat(auto-fill, minmax(180px, 1fr))",
+                        gap: "16px",
+                    }}
+                >
+                    {searchMovies?.map((movie) => (
+                        <Movie {...movie} key={movie.id} />
+                    ))}
                 </div>
             )}
         </div>
