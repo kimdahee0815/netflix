ea016f6502544e1fdcae692a70ae072b12e8fbbe
kimdahee0815 <kimdahee0815@gmail.com>
2026-06-28 20:30:14 +0900

    chore: console log delete

--- frontend/src/components/FavMovieList.js
+++ frontend/src/components/FavMovieList.js
@@ -7,77 +7,76 @@ import config from "../config";
 import { FavListUpdateProvider } from "../store/FavListTriggerContext";
 
 const FavMovieList = () => {
-    const id = window.sessionStorage.getItem("id");
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [favListState, setFavListState] = useState(false);
+  const id = window.sessionStorage.getItem("id");
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [favListState, setFavListState] = useState(false);
 
-    useEffect(() => {
-        axios
-            .get(`${config.API_URL}/favmovie/select?member_id=${id}`)
-            .then((res) => {
-                const movies = [...res.data];
-                console.log(movies);
-                setMovies(movies);
-                setLoading(false);
-            })
-            .catch((e) => {
-                console.error(e);
-            });
-    }, [favListState, id]);
+  useEffect(() => {
+    axios
+      .get(`${config.API_URL}/favmovie/select?member_id=${id}`)
+      .then((res) => {
+        const movies = [...res.data];
+        setMovies(movies);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, [favListState, id]);
 
-    const favListUpdate = () => {
-        setFavListState((favListState) => !favListState);
-    };
+  const favListUpdate = () => {
+    setFavListState((favListState) => !favListState);
+  };
 
-    const ctxValue = {
-        favListState,
-        favListUpdate,
-    };
+  const ctxValue = {
+    favListState,
+    favListUpdate,
+  };
 
-    return (
-        <FavListUpdateProvider value={ctxValue}>
-            {loading ? (
-                <div
-                    style={{
-                        color: "white",
-                        fontSize: "2.4em",
-                        marginLeft: "30px",
-                    }}
-                >
-                    loading...
-                </div>
-            ) : (
-                <div style={{ marginTop: "100px" }}>
-                    {movies.length ? (
-                        <Grid container spacing={2} justifyContent="center">
-                            {movies.map((movie) => (
-                                <Grid item xs={2}>
-                                    <Movie
-                                        key={`${movie.movie_title}`}
-                                        title={movie.movie_title}
-                                        summary={movie.movie_summary}
-                                        medium_cover_image={movie.movie_image}
-                                        isChecked={true}
-                                    />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    ) : (
-                        <div
-                            style={{
-                                color: "white",
-                                fontSize: "2.4em",
-                                marginLeft: "30px",
-                            }}
-                        >
-                            찜목록이 없습니다.
-                        </div>
-                    )}
-                </div>
-            )}
-        </FavListUpdateProvider>
-    );
+  return (
+    <FavListUpdateProvider value={ctxValue}>
+      {loading ? (
+        <div
+          style={{
+            color: "white",
+            fontSize: "2.4em",
+            marginLeft: "30px",
+          }}
+        >
+          loading...
+        </div>
+      ) : (
+        <div style={{ marginTop: "100px" }}>
+          {movies.length ? (
+            <Grid container spacing={2} justifyContent="center">
+              {movies.map((movie) => (
+                <Grid item xs={2}>
+                  <Movie
+                    key={`${movie.movie_title}`}
+                    title={movie.movie_title}
+                    summary={movie.movie_summary}
+                    medium_cover_image={movie.movie_image}
+                    isChecked={true}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <div
+              style={{
+                color: "white",
+                fontSize: "2.4em",
+                marginLeft: "30px",
+              }}
+            >
+              찜목록이 없습니다.
+            </div>
+          )}
+        </div>
+      )}
+    </FavListUpdateProvider>
+  );
 };
 
 export default FavMovieList;
